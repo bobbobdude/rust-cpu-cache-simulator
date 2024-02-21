c9a6eae693a40af363353528756eaafab024dc40
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-02-21 15:50:58 +0100

    I have implemented the following functionality:
    Take arguments from the command line and put them into a vector
    Identify which parts of the vector contain which parts of the information I need
    Assigned this to the appropriate variables
    Printed this to the screen for debugging
    Handled the error that occurs when a user inputs too many or too few arguments
    
    Below is an example of how my program currently works.
    
    This is provided to the terminal:
    
    cargo run -- -s 4 -E 1 -b 4 -t traces/yi.trace
    
    This is what the vector would look like following the above input:
    
    ["-s", "4", "-E", "1", "-b", "4", "-t", "traces/yi.trace"]
    
    And finally this is what the program identifies as the various relevant indexes and numerical values as well as the path to to the trace file the user wants to simulate:
    
    Index of -s string: 0, Number value of s: 4
    Index of -E string: 2, Number value of E: 1
    Index of -b string: 4, Number value of b: 4
    Index of -t string: 4, Path to trace file: traces/yi.trace

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,7 +1,46 @@
+#![allow(unused)]
+
+
+use std::env;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
+
+
 pub fn main() {
+    
+let args: Vec<String> = env::args().skip(1).collect(); //The skip here stops the executable binary being placed in the output
 
-    println!("Hello world!");
+println!("{:?}", args);
 
-    //Just testing if the repo is set up correctly 
+//This just ensures the correct number of command line arguments are provided and provides the user with a usage guide if incorrect
+//The max amount of arguments (if using the verbose and or helper flag) is 9 whereas the minimum is 8
+if args.len() < 8 || args.len() > 9 { 
+    println!("Usage: [-hv] -s <s> -E <E> -b <b> -t <tracefile>");
+    return;
+}
+
+let index_of_s = args.iter().position(|find_s: &String| find_s == "-s").unwrap();
+
+#[allow(non_snake_case)] //Although I know this goes against variable naming conventions as we are dealing with a capital E I think it is justified to defy these conventions for clarity
+let index_of_E = args.iter().position(|find_E: &String| find_E == "-E").unwrap();
+
+let index_of_b = args.iter().position(|find_b: &String| find_b == "-b").unwrap();
+let index_of_t = args.iter().position(|find_t: &String| find_t == "-t").unwrap();
+
+let value_of_s = &args[index_of_s + 1];
+#[allow(non_snake_case)]
+let value_of_E = &args[index_of_E + 1];
+let value_of_b = &args[index_of_b + 1];
+let path_to_trace = &args[index_of_t + 1];
+
+
+
+println!("Index of -s string: {}, Number value of s: {}",index_of_s, value_of_s);
+
+println!("Index of -E string: {}, Number value of E: {}",index_of_E, value_of_E);
+
+println!("Index of -b string: {}, Number value of b: {}",index_of_b, value_of_b);
+
+println!("Index of -t string: {}, Path to trace file: {}",index_of_b, path_to_trace);
 
 }
