529a55599e822d0f4a4f20b758beaa7c791893d0
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-02-22 11:25:58 +0100

    Created a function that takes the string references, converts them into i32 integers, multiplies them together following this formula to calculate the byte size of the cache:
    2ˢ * 2ᵇ * E
    
    This uses the .pow function which requires an unsigned integer, and as such I use the method try_into in order to attempt converting the i32 into an unsigned int, with the program panicking if I am unable to do so (these numbers should always be positive in any case).
    
    I then print the result to the screen which in the following trace:
    
    -s 4 -E 1 -b 4 -t traces/yi.trace
    
    results in a 256 byte cache.

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -27,11 +27,11 @@ let index_of_E = args.iter().position(|find_E: &String| find_E == "-E").unwrap()
 let index_of_b = args.iter().position(|find_b: &String| find_b == "-b").unwrap();
 let index_of_t = args.iter().position(|find_t: &String| find_t == "-t").unwrap();
 
-let value_of_s = &args[index_of_s + 1];
+let value_of_s: &String = &args[index_of_s + 1];
 #[allow(non_snake_case)]
-let value_of_E = &args[index_of_E + 1];
-let value_of_b = &args[index_of_b + 1];
-let path_to_trace = &args[index_of_t + 1];
+let value_of_E:&String = &args[index_of_E + 1];
+let value_of_b:&String = &args[index_of_b + 1];
+let path_to_trace:&String = &args[index_of_t + 1];
 
 
 
@@ -42,5 +42,22 @@ println!("Index of -E string: {}, Number value of E: {}",index_of_E, value_of_E)
 println!("Index of -b string: {}, Number value of b: {}",index_of_b, value_of_b);
 
 println!("Index of -t string: {}, Path to trace file: {}",index_of_b, path_to_trace);
+
+#[allow(non_snake_case)]
+//This dynamically calculates how many BYTES the combined cache is
+fn calculate_cache_size(s: &String, E:&String, b:&String) -> i32{
+    let int_of_s: i32= s.parse().unwrap();
+    let int_of_E: i32= E.parse().unwrap();
+    let int_of_b: i32= b.parse().unwrap();
+
+    let cache_bytes_size = (2_i32.pow(int_of_s.try_into().unwrap())) * (2_i32.pow(int_of_b.try_into().unwrap())) * int_of_E;
+
+    cache_bytes_size
+
+}
+
+let cache_bytes_size = calculate_cache_size(value_of_s, value_of_E, value_of_b);
+
+println!("The size of the cache is {} bytes", cache_bytes_size)
 
 }
