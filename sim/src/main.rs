6142b3ee4ffca5f3f776c7c98cf9ec2595e5cbb2
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-03-26 13:51:25 +0100

    Reduced lines needed and also implemented insert_into_cache_dmc which handles the direct mapped cache eventuality (but only store and load as of now - will implement modify now)

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -1,65 +1,42 @@
-#![allow(unused)]
-
-
-use std::alloc::System;
-use std::env;
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-use std::collections::HashMap;
- 
+#[allow(unused)]
+use std::{alloc::System, env, fs::File, io::{self, BufRead, BufReader}, collections::HashMap};
 #[macro_use]
 extern crate maplit;
+fn main() {
 
-pub fn main() {
-
-    
-    
 let args: Vec<String> = env::args().skip(1).collect(); //The skip here stops the executable binary being placed in the output
 
 println!("{:?}", args);
 
-//This just ensures the correct number of command line arguments are provided and provides the user with a usage guide if incorrect
-//The max amount of arguments (if using the verbose and or helper flag) is 9 whereas the minimum is 8
-if args.len() < 8 || args.len() > 9 { 
-    println!("Usage: [-hv] -s <s> -E <E> -b <b> -t <tracefile>");
+if args.len() < 8 || args.len() > 9 { //The max amount of arguments (if using the verbose and or helper flag) is 9 whereas the minimum is 8
+
+    println!("Usage: [-hv] -s <s> -E <E> -b <b> -t <tracefile>"); //This just ensures the correct number of command line arguments are provided and provides the user with a usage guide if incorrect
+
     return;
 }
 
 let index_of_s = args.iter().position(|find_s: &String| find_s == "-s").unwrap();
-
 #[allow(non_snake_case)] //Although I know this goes against variable naming conventions as we are dealing with a capital E I think it is justified to defy these conventions for clarity
 let index_of_E = args.iter().position(|find_E: &String| find_E == "-E").unwrap();
-
 let index_of_b = args.iter().position(|find_b: &String| find_b == "-b").unwrap();
 let index_of_t = args.iter().position(|find_t: &String| find_t == "-t").unwrap();
-
 let value_of_s: &String = &args[index_of_s + 1];
 #[allow(non_snake_case)]
 let value_of_E:&String = &args[index_of_E + 1];
 let value_of_b:&String = &args[index_of_b + 1];
 let path_to_trace:&String = &args[index_of_t + 1];
-
-
-
 println!("Index of -s string: {}, Number value of s: {}",index_of_s, value_of_s);
-
 println!("Index of -E string: {}, Number value of E: {}",index_of_E, value_of_E);
-
 println!("Index of -b string: {}, Number value of b: {}",index_of_b, value_of_b);
-
 println!("Index of -t string: {}, Path to trace file: {}",index_of_t, path_to_trace);
 
 #[allow(non_snake_case)]
-//This dynamically calculates how many BYTES the combined cache is
-fn calculate_cache_size(s: &String, E:&String, b:&String) -> u32{
+fn calculate_cache_size(s: &String, E:&String, b:&String) -> u32{ //This dynamically calculates how many BYTES the combined cache is
     let int_of_s: u32= s.parse().unwrap(); //This is the amount of Sets 
     let int_of_E: u32= E.parse().unwrap(); //This is the amount of Cache Lines per set 
     let int_of_b: u32= b.parse().unwrap(); //This is the block offset (the amount of bits that determine which byte to take from the requested block)
-
     let cache_bytes_size: u32 = (2_u32.pow(int_of_s)) * (2_u32.pow(int_of_b)) * int_of_E;
-
     cache_bytes_size
-
 }
 
 fn make_file_line_separated_vector(filepath: &str) -> Vec<String>{
@@ -74,8 +51,7 @@ fn make_file_line_separated_vector(filepath: &str) -> Vec<String>{
 }
 
 
-fn convert_from_hex_to_binary(hex_address: &str) -> Result<String, &'static str>{ //The function either returns a String or an error (as handled by &'static str) 
-//Just for future reference, the reason you chose a static lifetime is so the error message/string persists for the whole time the program executes as we might need to see at the last second that one of the strings failed to convert to binary
+fn convert_from_hex_to_binary(hex_address: &str) -> Result<String, &'static str>{ //The function either returns a String or an error (as handled by &'static str) Just for future reference, the reason you chose a static lifetime is so the error message/string persists for the whole time the program executes as we might need to see at the last second that one of the strings failed to convert to binary
     let my_map_of_hex_to_binary: HashMap<char, String> = hashmap! {
         '0' => "0000".to_string(),
         '1' => "0001".to_string(),
@@ -114,9 +90,7 @@ struct TupleOfTagAndAddress<'a>{
     binary: String
 }
 
-//if first_byte != 32{ remember the below function should only run if this if statement is satisfied as 32 is equivalent to a space 
-
-fn turn_line_sep_vector_into_tuple(line_of_vec: &str)-> Result<TupleOfTagAndAddress, &'static str>{
+fn turn_line_sep_vector_into_tuple(line_of_vec: &str)-> Result<TupleOfTagAndAddress, &'static str>{ //if first_byte != 32{ remember the below function should only run if this if statement is satisfied as 32 is equivalent to a space 
     let first_byte:i32 = line_of_vec.chars().next().unwrap() as i32;
     if first_byte == 32{
         let index_of_comma = line_of_vec.find(",").unwrap_or(line_of_vec.len()); //This tries to find the comma and if it fails it instead returns the length of the string 
@@ -142,17 +116,12 @@ if value_of_E == "1"{
     println!("This is a direct mapped cache")
 }
 
-
-
 let cache_bytes_size = calculate_cache_size(value_of_s, value_of_E, value_of_b);
-
 println!("The size of the cache is {} bytes", cache_bytes_size);
-
 let vec_of_trace_file = make_file_line_separated_vector(path_to_trace);
 
-//Okay so we need to split the binary address into the tag bits, set bits and block bits and store the type of address it is alongside it. 
 
-struct BinaryInTagSetBlockParts<>{
+struct BinaryInTagSetBlockParts<>{//Okay so we need to split the binary address into the tag bits, set bits and block bits and store the type of address it is alongside it. 
     type_of_mem_access: String,
     tag_bits: String,
     set_bits: String,
@@ -193,32 +162,6 @@ fn split_binary_address_into_type_t_s_and_b(vec_of_trace_file_input: Vec<String>
 }
 
 let mut vec_of_binary_split_memory_addresses = split_binary_address_into_type_t_s_and_b(vec_of_trace_file, value_of_s,value_of_b).unwrap();
-
-
-
-
-
-
-
-        //println!("The index of tag bits are 0..{:?}", set_start);
-        //println!("The index of set bits are {:?}..{:?}", set_start, set_end);
-        //println!("The index of block bits are {:?}..{:?}", block_start, block_end);
-        //println!();
-        //println!("{}", cloned_binary);
-        //println!("Type of memory access {}, the tag bits {}, the set bits {}, the block bits {}", binary_split_memory_address_to_input.type_of_mem_access, binary_split_memory_address_to_input.tag_bits, binary_split_memory_address_to_input.set_bits, binary_split_memory_address_to_input.block_bits);
-    
-    //println!("Type of memory access {}, the tag bits {}, the set bits {}, the block bits {}", binary_split_memory_address_to_input.type_of_mem_access, binary_split_memory_address_to_input.tag_bits, binary_split_memory_address_to_input.set_bits, binary_split_memory_address_to_input.block_bits);
-    //println!(); 
-    //println!("Value of binary length {}, value of s {}, value of b {}", length_of_binary, value_of_s_as_num, value_of_b_as_num);
-
-    /*
-        for binary_split_memory_address in vec_of_binary_split_memory_addresses{
-             println!("Type of memory access {}, the tag bits {}, the set bits {}, and the size in bytes is {}", binary_split_memory_address.type_of_mem_access, binary_split_memory_address.tag_bits, binary_split_memory_address.set_bits, binary_split_memory_address.size );
-             println!();
-    }
-     */
-
-
     //Okay so to represent the cache sets, and the amount of cache lines within those sets I have decided to create a fixed size 2d array. 
     let cache_sets: usize = 2_usize.pow(value_of_s.parse().unwrap()); //rows
     let cache_lines: usize = value_of_E.to_string().parse().unwrap(); //columns add one as we need a column for the block stored within the cache 
@@ -233,31 +176,15 @@ let mut vec_of_binary_split_memory_addresses = split_binary_address_into_type_t_
     }
 
     //https://rust-unofficial.github.io/patterns/idioms/ctor.html
-
-    //Equivalent of constructor in Java
     
     impl ArrayRepresentationOfCache{ 
-        fn new(rows_or_cache_sets: usize, cols_or_cache_lines: usize)-> Self{
+        fn new(rows_or_cache_sets: usize, cols_or_cache_lines: usize)-> Self{//Equivalent of constructor in Java
             let two_d_array = vec![vec!["empty".to_string();cols_or_cache_lines + 1];rows_or_cache_sets]; //columns/cache lines add one as we need a column for the block stored within the cache 
             let initial_value_of_all_counters = 0;
             Self {rows_or_cache_sets, cols_or_cache_lines, two_d_array, cache_hits:initial_value_of_all_counters, cache_misses:initial_value_of_all_counters, cache_evictions:initial_value_of_all_counters}
         }
     }
 
-    impl ArrayRepresentationOfCache{
-        fn to_add_without_eviction(&mut self, set_bits: String, tag_bits: String){
-
-            if (&mut self.two_d_array.len() == &mut self.rows_or_cache_sets){
-                for row in &mut self.two_d_array{
-                    if row[0] == "empty".to_string(){
-                        row[0] = set_bits.clone();
-                        row[1] = tag_bits.clone(); 
-                    }
-                }
-            }
-
-        }
-    }
 
     impl ArrayRepresentationOfCache{
         fn is_set_in_the_cache(&mut self, set_bits: String) -> Option<usize>{
@@ -284,6 +211,7 @@ let mut vec_of_binary_split_memory_addresses = split_binary_address_into_type_t_
             else{
                 //remember this will also cause a cache eviction in a DMC but think I should add that at the point where the data is actually evicted from the cache. 
                 self.cache_misses += 1;
+                self.cache_evictions += 1;
                 return false;
             }
         }
@@ -308,6 +236,35 @@ let mut vec_of_binary_split_memory_addresses = split_binary_address_into_type_t_
                 }
             }
             return false;
+            self.cache_misses += 1;
+            self.cache_evictions += 1; 
+        }
+    }
+
+    impl ArrayRepresentationOfCache{
+        fn insert_into_cache_dmc(&mut self, set_bits: String, tag_bits: String){ //dmc is direct mapped cache
+            let is_set_in_cache = self.is_set_in_the_cache(set_bits.clone());
+            //Below handles both store and load instructions
+            if let Some(index_of_set) = is_set_in_cache{ //Unsure about how this bit works but looked at error and changed it according to Rust, I think this only evaluates to true if "is_set_in_cache" returns "Some" as opposed to "None"
+                let do_tag_bits_match = self.check_if_tag_bits_match(tag_bits.clone(), index_of_set);
+                if do_tag_bits_match == true{
+                    self.two_d_array.remove(index_of_set); //Has to be first as if we insert another element we may fuck up the index 
+                    self.two_d_array.push(vec![set_bits, tag_bits]);
+                }
+                else if do_tag_bits_match != true{
+                    self.two_d_array.remove(index_of_set);
+                    self.two_d_array.push(vec![set_bits, tag_bits]);
+                }
+            }
+            else{
+                if self.is_there_an_empty_cache_line() == true{
+                    self.two_d_array.push(vec![set_bits, tag_bits]);
+                }
+                else if self.is_there_an_empty_cache_line() == false{
+                    self.two_d_array.pop();
+                    self.two_d_array.push(vec![set_bits, tag_bits]);
+                }
+            }
         }
     }
     
@@ -315,30 +272,10 @@ let mut vec_of_binary_split_memory_addresses = split_binary_address_into_type_t_
 
     let mut test_of_cache_struct = ArrayRepresentationOfCache::new(cache_sets, cache_lines);
 
-    let test = test_of_cache_struct.is_there_an_empty_cache_line();
+    for binary in vec_of_binary_split_memory_addresses{
+        test_of_cache_struct.insert_into_cache_dmc(binary.set_bits, binary.tag_bits);
+    }
 
-    println!("{}", test);
-
-// Okay take the example where I have an address line in binary that looks like this 011111111110111111100000010110101000: 
-//
-// This is a 36-bit address line sent from the CPU. 
-//
-// Let's imagine this is a direct mapped cache with 16 sets (and as its direct mapped one cache line per set).
-//
-// The block offset (which in my case can be ignored as we are only simulating cache hits and misses) is the first four bits, the set bits are the next four bits after. 
-//
-// So, in this case lets imagine the cache is empty, and that this is a load operation.
-//
-// First, we would check of the cache contains any data within that set and if it doesn't, we consider it a cache miss (compulsory). 
-//
-// Then in our imaginary cache we store the set bits and the tag, as the tag determines which block is stored in the cache set. 
-//
-// If we have another address that loads the same block in the same set, it would be a cache hit. 
-//
-// But if the CPU sends a request to the same set but for a different block, we will have a cache miss (conflict miss).
-//
-// In the situation that the set bits are different we would store the following block in the imaginary cache in a different cache set. 
-//
-// Throughout all of these processes we need to check whether the cache is full (or if any of the cache lines are not "empty")
+    println!("hits:{} misses:{} evictions:{}", test_of_cache_struct.cache_hits, test_of_cache_struct.cache_misses, test_of_cache_struct.cache_evictions);
 
 }
