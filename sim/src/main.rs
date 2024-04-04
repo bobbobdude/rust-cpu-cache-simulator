d34464c262e36460b90a677b1b429a57c454b663
bobbobdude <thomasrobertgardner1056@gmail.com>
2024-04-04 10:11:10 +0200

    Moved all the functions out of the main file in order to facilitate testing in the main fail, added some more fancy tests (one with a match statement OooOOOO) and removed some unused variables.

--- sim/src/main.rs
+++ sim/src/main.rs
@@ -34,36 +34,6 @@ fn make_file_line_separated_vector(filepath: &str) -> Vec<String>{
     return vec_of_lines;
 }
 
-#[allow(unused)]
-fn main() {
-
-let args: Vec<String> = env::args().skip(1).collect(); //The skip here stops the executable binary being placed in the output
-
-
-if args.len() < 8 || args.len() > 9 { //The max amount of arguments (if using the verbose and or helper flag) is 9 whereas the minimum is 8
-
-    println!("Usage: [-hv] -s <s> -E <E> -b <b> -t <tracefile>"); //This just ensures the correct number of command line arguments are provided and provides the user with a usage guide if incorrect
-
-    return;
-}
-
-let index_of_s = args.iter().position(|find_s: &String| find_s == "-s").unwrap();
-#[allow(non_snake_case)] //Although I know this goes against variable naming conventions as we are dealing with a capital E I think it is justified to defy these conventions for clarity
-let index_of_E = args.iter().position(|find_E: &String| find_E == "-E").unwrap();
-let index_of_b = args.iter().position(|find_b: &String| find_b == "-b").unwrap();
-let index_of_t = args.iter().position(|find_t: &String| find_t == "-t").unwrap();
-let value_of_s: &String = &args[index_of_s + 1];
-#[allow(non_snake_case)]
-let value_of_E:&String = &args[index_of_E + 1];
-let value_of_b:&String = &args[index_of_b + 1];
-let path_to_trace:&String = &args[index_of_t + 1];
-
-
-
-
-
-
-
 fn convert_from_hex_to_binary(hex_address: &str) -> Result<String, &'static str>{ //The function either returns a String or an error (as handled by &'static str) Just for future reference, the reason you chose a static lifetime is so the error message/string persists for the whole time the program executes as we might need to see at the last second that one of the strings failed to convert to binary
     let my_map_of_hex_to_binary: HashMap<char, String> = hashmap! {
         '0' => "0000".to_string(),
@@ -94,12 +64,10 @@ fn convert_from_hex_to_binary(hex_address: &str) -> Result<String, &'static str>
 }
     Ok(binary)
     
-} 
+}
 
 struct TupleOfTagAndAddress<'a>{
     tag: &'a str,
-    hex_address: &'a str,
-    size: &'a str,
     binary: String
 }
 
@@ -107,13 +75,10 @@ fn turn_line_sep_vector_into_tuple(line_of_vec: &str)-> Result<TupleOfTagAndAddr
     let first_byte:i32 = line_of_vec.chars().next().unwrap() as i32;
     if first_byte == 32{
         let index_of_comma = line_of_vec.find(",").unwrap_or(line_of_vec.len()); //This tries to find the comma and if it fails it instead returns the length of the string 
-        let index_of_size = index_of_comma + 1;
         let line_of_vec_with_size_removed: &str = &line_of_vec[0..index_of_comma];
         let split_instruction: Vec<&str> = line_of_vec_with_size_removed.split_whitespace().collect();
         let tag_address = TupleOfTagAndAddress{
             tag: split_instruction[0],
-            hex_address: split_instruction[1],
-            size: &line_of_vec[index_of_size..],
             binary: convert_from_hex_to_binary(split_instruction[1]).unwrap() 
         };
         Ok(tag_address)
@@ -122,21 +87,12 @@ fn turn_line_sep_vector_into_tuple(line_of_vec: &str)-> Result<TupleOfTagAndAddr
             return Err("First character is not a space and therefore it is ignored")
         }
     } 
-    
 
-
-
-
-let cache_bytes_size = calculate_cache_size(value_of_s, value_of_E, value_of_b);
-let vec_of_trace_file = make_file_line_separated_vector(path_to_trace);
-
-
-struct BinaryInTagSetBlockParts<>{//Okay so we need to split the binary address into the tag bits, set bits and block bits and store the type of address it is alongside it. 
-    type_of_mem_access: String,
-    tag_bits: String,
-    set_bits: String,
-    size: String
-}
+    struct BinaryInTagSetBlockParts<>{//Okay so we need to split the binary address into the tag bits, set bits and block bits and store the type of address it is alongside it. 
+        type_of_mem_access: String,
+        tag_bits: String,
+        set_bits: String,
+    }
 
 fn split_binary_address_into_type_t_s_and_b(vec_of_trace_file_input: Vec<String>, value_of_s: &String, value_of_b: &String)-> Result<Vec<BinaryInTagSetBlockParts>, &'static str>{
     if !vec_of_trace_file_input.is_empty(){
@@ -151,11 +107,10 @@ fn split_binary_address_into_type_t_s_and_b(vec_of_trace_file_input: Vec<String>
 
             let block_bits_end_index = length_of_binary - value_of_b_as_num; 
 
-            let mut binary_split_memory_address_to_input = BinaryInTagSetBlockParts{
+            let binary_split_memory_address_to_input = BinaryInTagSetBlockParts{
                 type_of_mem_access: input_tuple.tag.to_string(),
                 tag_bits: cloned_binary[0..block_bits_end_index-value_of_s_as_num].to_string(), 
-                set_bits: cloned_binary[block_bits_end_index-value_of_s_as_num..block_bits_end_index].to_string(), 
-                size: input_tuple.size.to_string()
+                set_bits: cloned_binary[block_bits_end_index-value_of_s_as_num..block_bits_end_index].to_string(),
             };
 
             // println!("Original full binary: {}, Original hex address: {}, Tag bits: {}, Set bits: {}",&input_tuple.binary, &input_tuple.hex_address, &binary_split_memory_address_to_input.tag_bits, binary_split_memory_address_to_input.set_bits);
@@ -170,6 +125,53 @@ fn split_binary_address_into_type_t_s_and_b(vec_of_trace_file_input: Vec<String>
     }
 
 }
+
+#[allow(unused)]
+fn main() {
+
+let args: Vec<String> = env::args().skip(1).collect(); //The skip here stops the executable binary being placed in the output
+
+
+if args.len() < 8 || args.len() > 9 { //The max amount of arguments (if using the verbose and or helper flag) is 9 whereas the minimum is 8
+
+    println!("Usage: [-hv] -s <s> -E <E> -b <b> -t <tracefile>"); //This just ensures the correct number of command line arguments are provided and provides the user with a usage guide if incorrect
+
+    return;
+}
+
+let index_of_s = args.iter().position(|find_s: &String| find_s == "-s").unwrap();
+#[allow(non_snake_case)] //Although I know this goes against variable naming conventions as we are dealing with a capital E I think it is justified to defy these conventions for clarity
+let index_of_E = args.iter().position(|find_E: &String| find_E == "-E").unwrap();
+let index_of_b = args.iter().position(|find_b: &String| find_b == "-b").unwrap();
+let index_of_t = args.iter().position(|find_t: &String| find_t == "-t").unwrap();
+let value_of_s: &String = &args[index_of_s + 1];
+#[allow(non_snake_case)]
+let value_of_E:&String = &args[index_of_E + 1];
+let value_of_b:&String = &args[index_of_b + 1];
+let path_to_trace:&String = &args[index_of_t + 1];
+
+
+
+
+
+
+
+ 
+
+
+
+
+    
+
+
+
+
+let cache_bytes_size = calculate_cache_size(value_of_s, value_of_E, value_of_b);
+let vec_of_trace_file = make_file_line_separated_vector(path_to_trace);
+
+
+
+
 
 let mut vec_of_binary_split_memory_addresses = split_binary_address_into_type_t_s_and_b(vec_of_trace_file.clone(), value_of_s,value_of_b).unwrap();
     //Okay so to represent the cache sets, and the amount of cache lines within those sets I have decided to create a fixed size 2d array. 
@@ -258,16 +260,27 @@ let cache_lines: usize = value_of_E.to_string().parse().unwrap(); //columns add 
         }
 
         #[test]
+        #[should_panic]
         fn test_make_file_line_separated_vector_invalid_filepath(){
-            let line_sep_vec = make_file_line_separated_vector("src/traces/DOESNOTEXIST/custom.trace");
+            make_file_line_separated_vector("src/traces/DOESNOTEXIST/custom.trace"); //Will pass only if it DOES panic 
+
         }
     
-        // #[test]
-        // fn test_hex_to_binary() {
-        //     let hex = "1a3f";
-        //     let binary = convert_from_hex_to_binary(hex).unwrap();
-        //     assert_eq!(binary, "0001101000111111");
-        // }
-    
+        #[test]
+        fn test_hex_to_binary() {
+            let hex = "1a3f";
+            let binary = convert_from_hex_to_binary(hex).unwrap();
+            assert_eq!(binary, "0001101000111111");
+        }
+
+        #[test]
+        fn test_hex_to_binary_invalid_input(){
+            let invalid_hex = "z";
+            match convert_from_hex_to_binary(invalid_hex){
+                Ok(_) => panic!("Expected an error for invalid hex input, but got nada."),
+                Err(e) => assert_eq!(e, "Hex character not valid", "Unexpected error message for invalid hex input.")
+            }
+        }
+
     }
 
